63bd9284a35bc824887886ded6cb52dbf67e07a4
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-17 21:58:30 +0300

    Task 6

---
+++ js/task6.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector("#validation-input");
+const dataLengthEl = textInput.getAttribute("data-length");
+
+textInput.addEventListener("blur", (event) => {
+  const stringLength = event.currentTarget.value.length;
+  console.log(stringLength);
+  if (stringLength === +dataLengthEl) {
+    event.currentTarget.classList.add("valid");
+    event.currentTarget.classList.remove("invalid");
+  } else {
+    event.currentTarget.classList.add("invalid");
+  }
+});
