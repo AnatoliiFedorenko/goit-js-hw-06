d81f652fdc6d968b2b014238adac916f54895acf
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-18 22:00:15 +0300

    fixed 5

--- js/task5.js
+++ js/task5.js
@@ -4,6 +4,7 @@ const output = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
   if (event.currentTarget.value === "") {
     output.textContent = "Anonymous";
+    return;
   }
   output.textContent = event.currentTarget.value;
 });
